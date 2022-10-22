752a5d57b67b1c2f497bb2e6d8a3d15692785c04
PatB1234 <80209204+PatB1234@users.noreply.github.com>
2022-10-22 13:59:01 +0300

    Started on API and testing Formally

--- src/api.py
+++ src/api.py
@@ -1,3 +1,4 @@
+from src import sqldb
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -23,3 +24,57 @@ def redirect_home(request: Request):
 def get_login(request: Request):
 
     return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/admin_dash")
+def get_admin_dash(request: Request):
+
+    return templates.TemplateResponse("AdminDashboard.html", {"request": request})
+
+@app.post("/login")
+def post_login(request: Request, username: str = Form(...), password: str = Form(...)):
+
+    student = sqldb.get_student_object_from_username_and_password(username, password)
+    admin = sqldb.get_admin_object_from_username_and_password(username, password)
+    if (student == sqldb.BLANK_STUDENT and admin == sqldb.BLANK_ADMIN):
+
+        return RedirectResponse("/login?error=True", status_code=status.HTTP_401_UNAUTHORIZED)
+
+@app.get("/students")
+def get_students(request: Request):
+
+    return sqldb.get_students()
+
+@app.post("/add_student")
+def post_add_student(name: str = Form(...), password: str = Form(...), studentClass: str = Form(...), studentID: int = Form(...)):
+
+    sqldb.add_student(name, password, studentClass, studentID)
+
+@app.post("/delete_student")
+def post_delete_student(studentID: int = Form(...)):
+
+    sqldb.delete_student(studentID)
+
+@app.get("/get_messages")
+def get_messages():
+
+    return sqldb.get_messages()
+
+@app.get("/get_chats")
+def get_chats(uid: int = Form(...)):
+
+    return sqldb.get_messages_for_user(uid)
+
+@app.post("/add_message")
+def post_add_message(sender: str = Form(...), senderID: int = Form(...), receiverID: int = Form(...), message: str = Form(...)):
+
+    sqldb.add_message(message, sender, senderID, receiverID)
+
+@app.post("/report_bullying")
+def post_report_bullying(sender: str = Form(...), senderID: int = Form(...), cause: str = Form(...), causeID: int = Form(...), controlledBy: str = Form(...), controlledByID: int = Form(...), completed: bool = Form(...)):
+
+    sqldb.add_message(sender, senderID, cause, causeID, controlledBy, controlledByID, completed)
+
+@app.post("/create_chat")
+def post_create_chat(person1: str = Form(...), person1ID: str = Form(...), person2: str = Form(...), person2ID:str = Form(...), classroomID: int = Form(...)):
+
+    sqldb.create_chat(person1, person1ID, person2, person2ID, classroomID)
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -5,6 +5,16 @@ from fastapi.testclient import TestClient
 client = TestClient(api.app)
 
 def test_get_login():
+
     response = client.get("/login")
     assert response.status_code == 200
-    #assert response.html() == "empty"
+    
+def test_post_login1():
+
+    response = client.post("/login", data={"username": "LMAO", "password": "LMAO"})
+    assert response.status_code == 200
+
+def test_post_login2():
+
+    response = client.post("/login", data={"username": "asdf", "password": "aa"})
+    assert response.status_code == 401
